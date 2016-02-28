81d6589180b0e9c4091db7060ee349c9fc954a3f
Kathleen House <kathleenhouse@Macintosh-141.local>
2016-02-28 17:57:01 -0500

    added the set time interval back in

--- templates.js
+++ templates.js
@@ -1,9 +1,14 @@
-var templates = {
-  toDoList: [
-  '<div data-idx="<%= idx %>">',
-  '<p><%= post %></p>',
+var templates = {};
+
+templates.toDoList = [
+  // '<% if(completed) {%>',
+  // '<div class="completed"',
+  // '<%} else {%>',
+  // '<div class="active"',
+  // '<%} %>',
+  '<div data-idx="<%= idx %>"><i class="fa fa-check-circle-o fa-2x" id="check"></i>',
+  '<%= post %>',
   '<button type="button" name="Delete" class="delete-post">Delete</button>',
   // '<button type="button" name="Edit" class="editList"></button>',
   '</div>'
 ].join("")
-};
